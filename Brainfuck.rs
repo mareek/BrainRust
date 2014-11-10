7f51548393f1eb976468719465bade44f1187121
Matthieu Mourisson <mareek@gmail.com>
2014-11-11 00:21:43 +0100

    The interpreter works.

--- Brainfuck.rs
+++ Brainfuck.rs
@@ -1,3 +1,13 @@
+// http://esolangs.org/wiki/Brainfuck
+//  >  Increment the pointer.
+//  <  Decrement the pointer
+//  +  Increment the byte at the pointer.
+//  -  Decrement the byte at the pointer.
+//  .  Output the byte at the pointer.
+//  ,  Input a byte and store it in the byte at the pointer.
+//  [  Jump forward past the matching ] if the byte at the pointer is zero.
+//  ]  Jump backward to the matching [ unless the byte at the pointer is zero.
+
 fn main() {
     println!("Start of program");
     println!("");
@@ -29,10 +39,10 @@ fn process_instruction (program : &str, tape : &mut[u8], pointer : &mut uint, po
         '<' => *pointer -= 1u,
         '+' => tape[*pointer] += 1u8,
         '-' => tape[*pointer] -= 1u8,
-        '.' => print!("{}", std::str::from_utf8([tape[*pointer]])),
+        '.' => output_current_value(tape, *pointer),
         ',' => panic!("Not implemented operation"),
-        '[' => panic!("Not implemented operation"),
-        ']' => panic!("Not implemented operation"),
+        '[' => process_opening_bracket(tape[*pointer], program, position),
+        ']' => process_closing_bracket(tape[*pointer], program, position),
         _ => {/* Ignore */}
     }
     
@@ -45,3 +55,35 @@ fn get_instruction(program : &str, pos : uint) -> char {
         None => panic!("At the disco")
     }
 }
+
+fn output_current_value(tape : &[u8], pointer : uint) {
+    let value = tape[pointer];
+    match std::str::from_utf8([value]) {
+        Some(c) => print!("{}", c),
+        None => {
+            println!("");
+            println!("Error, incorrect utf-8 value : {}", value);
+            println!("");
+        }
+    }    
+}
+
+
+//  [  Jump forward past the matching ] if the byte at the pointer is zero.
+fn process_opening_bracket (value : u8, program : &str, position : &mut uint) {
+    if value == 0 {
+        while get_instruction(program, *position) != ']' {
+            *position += 1;
+        }
+    }
+}
+
+//  ]  Jump backward to the matching [ unless the byte at the pointer is zero.
+fn process_closing_bracket (value : u8, program : &str, position : &mut uint) {
+    if value != 0 {
+        while get_instruction(program, *position) != '[' {
+            *position -= 1;
+        }
+    }
+}
+
