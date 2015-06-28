e35a569301ccdc426f76f92742e70465c54071f1
Matthieu Mourisson <mareek@gmail.com>
2015-06-28 18:00:35 +0200

    added  implementation of most instructions except loops

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs::File;
+use std::io;
 use std::io::Read;
 use std::path::Path;
+use std::str;
 
 // +    Increases element under pointer
 // -    Decrases element under pointer
@@ -22,7 +24,7 @@ fn main(){
     }
 }
 
-fn read_file<'a>(file_path : &Path) -> String {
+fn read_file(file_path : &Path) -> String {
     let mut buffer = String::new();
     match File::open(file_path) {
         Ok(mut file) => match file.read_to_string(&mut buffer) {
@@ -43,7 +45,7 @@ fn execute(program : &str)-> String {
 }
 
 trait CodeBlock {
-    fn execute(&self);
+    fn execute(&self, tape : &mut[u8], pointer : &mut usize);
 } 
 
 struct Program {
@@ -53,7 +55,7 @@ struct Program {
 }
 
 impl Program {
-    pub fn new(program: &str) -> Program {
+    fn new(program: &str) -> Program {
         Program { 
             raw: Vec::new(),
             instructions: Vec::new(),
@@ -63,9 +65,45 @@ impl Program {
 }
 
 impl CodeBlock for Program {
-    fn execute(&self) {
+    fn execute(&self, tape : &mut[u8], pointer : &mut usize) {
         for block in &self.blocks {
-            block.execute();
+            block.execute(tape, pointer);
         }
     }
+}
+
+impl CodeBlock for char {
+    fn execute(&self, tape : &mut[u8], pointer : &mut usize) {
+        match *self {
+            '>' => *pointer += 1,
+            '<' => *pointer -= 1,
+            '+' => tape[*pointer] += 1u8,
+            '-' => tape[*pointer] -= 1u8,
+            '.' => output(tape, *pointer),
+            ',' => input(tape, *pointer),
+            _ => {/* Ignore */}
+        }
+    }
+}
+
+fn output(tape : &[u8], pointer : usize) {
+    let value = tape[pointer];
+    match str::from_utf8(&[value]) {
+        Ok(c) => print!("{}", c),
+        Err(e) => print_error(format!("incorrect utf-8 value : {}", value).trim())
+    }    
+}
+
+fn input(tape : &mut[u8], pointer : usize) {
+    let buffer : &mut[u8] = &mut [0u8];
+    match io::stdin().read(buffer) {
+        Ok(_) => tape[pointer] = buffer[0],
+        Err(e) => panic!("no input : {}", e)
+    }
+}
+
+fn print_error(error_message : &str) {
+    println!("");
+    println!("Error, {}", error_message);
+    println!("");
 }
