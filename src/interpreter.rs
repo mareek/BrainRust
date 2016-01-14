3c0ea8599b41333bb4eee8cad88ac33a40af6924
Matthieu Mourisson <mareek@gmail.com>
2016-01-15 00:58:18 +0100

    The interpreter is now feature complete !

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,34 +1,33 @@
+use std::io;
 use std::io::Read;
+use std::io::Write;
 use std::str::Chars;
-use utils;
 
 pub trait CodeBlock {
-    fn execute(&self, tape : &mut[u8], pointer: &mut usize);
-} 
+    fn execute(&self, tape: &mut [u8], pointer: &mut usize);
+}
 
 pub struct Program {
     blocks: Vec<Box<CodeBlock>>
 }
 
 impl Program {
-    pub fn new(program: &String, output : fn(u8), input : fn() ->u8) -> Program {
+    pub fn new(program: &String) -> Program {
         let mut instructions = program.chars();
         let mut blocks = Vec::new();
         while let Some(instruction) = instructions.next() {
-            blocks.push(get_code_block(&mut instructions, instruction, output, input));
+            blocks.push(get_code_block(&mut instructions, instruction));
         }
-        Program { 
-            blocks: blocks
-        }
+        Program { blocks: blocks }
     }
 
     pub fn new_command_line(program: &String) -> Program {
-        Program::new(program, utils::output, utils::input)
-    }    
+        Program::new(program)
+    }
 }
 
 impl CodeBlock for Program {
-    fn execute(&self, tape : &mut[u8], pointer : &mut usize) {
+    fn execute(&self, tape: &mut [u8], pointer: &mut usize) {
         for block in &self.blocks {
             block.execute(tape, pointer);
         }
@@ -36,70 +35,73 @@ impl CodeBlock for Program {
 }
 
 struct Loop {
-    blocks: Vec<Box<CodeBlock>>
+    blocks: Vec<Box<CodeBlock>>,
 }
 
 impl Loop {
-    fn new(instructions: &mut Chars, output : fn(u8), input : fn() ->u8) -> Loop {
+    fn new(instructions: &mut Chars) -> Loop {
         let mut blocks = Vec::new();
-        while  let Some(instruction) = instructions.next() {
+        while let Some(instruction) = instructions.next() {
             if instruction == ']' {
                 break;
             } else {
-                blocks.push(get_code_block(instructions, instruction, output, input));
+                blocks.push(get_code_block(instructions, instruction));
             }
         }
-        
-        Loop {
-            blocks: blocks
-        }        
+
+        Loop { blocks: blocks }
     }
 }
 
 impl CodeBlock for Loop {
-    fn execute(&self, tape : &mut[u8], pointer : &mut usize) {
+    fn execute(&self, tape: &mut [u8], pointer: &mut usize) {
         while tape[*pointer] != 0u8 {
             for block in &self.blocks {
                 block.execute(tape, pointer);
             }
         }
     }
-} 
+}
 
 struct Instruction {
-    value : char,
-    output : fn(u8),
-    input : fn() -> u8
+    value: char
 }
 
 impl Instruction {
-    fn new(value: char,  output : fn(u8), input : fn() -> u8) -> Instruction {
-        Instruction {
-            value: value,
-            output: output, 
-            input: input 
-        }        
+    fn new(value: char) -> Instruction {
+        Instruction { value: value }
+    }
+
+    fn input(&self, tape: &mut [u8], pointer: &mut usize) {
+        let mut stdin = io::stdin();
+        stdin.read(&mut tape[*pointer..*pointer+1]).unwrap();
+    }
+
+    fn output(&self, tape: &mut [u8], pointer: &mut usize) {
+        let mut stdout = io::stdout();
+        stdout.write(&mut tape[*pointer..*pointer+1]).unwrap(); 
+        stdout.flush().unwrap() ;
     }
 }
 
 impl CodeBlock for Instruction {
-    fn execute(&self, tape : &mut[u8], pointer: &mut usize) {
+    fn execute(&self, tape: &mut [u8], pointer: &mut usize) {
         match self.value {
             '>' => *pointer += 1,
             '<' => *pointer -= 1,
             '+' => tape[*pointer] += 1u8,
             '-' => tape[*pointer] -= 1u8,
-            '.' => self.output(tape[*pointer]),
-            ',' => tape[*pointer] = self.input(),
-            _ => {/* Ignore */}
+            '.' => self.output(tape, pointer),
+            ',' => self.input(tape, pointer),
+            _ => { /* Ignore */ }
         }
     }
 }
 
-fn get_code_block(instructions: &mut Chars, instruction: char, output : fn(u8), input : fn() ->u8) -> Box<CodeBlock> {
+fn get_code_block(instructions: &mut Chars, instruction: char) -> Box<CodeBlock> {
     if instruction == '[' {
-        Box::new(Loop::new(instructions, output, input))
+        Box::new(Loop::new(instructions))
     } else {
-        Box::new(Instruction::new(instruction, output, input)) 
+        Box::new(Instruction::new(instruction))
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,6 @@
 use std::fs::File;
-use std::io;
 use std::io::Read;
 use std::path::Path;
-use std::str;
 
 pub fn read_file(file_path : &Path) -> String {
     let mut buffer = String::new();
@@ -13,23 +11,4 @@ pub fn read_file(file_path : &Path) -> String {
         },
         Err(e) => panic!("invalid file : {}. {}", file_path.display(), e)
     }
-}
-
-pub fn output(value : u8) {
-    match str::from_utf8(&[value]) {
-        Ok(c) => print!("{}", c),
-        Err(_) => print_error(format!("incorrect utf-8 value : {}", value).trim())
-    }    
-}
-
-pub fn input() -> u8 {
-    let mut buffer = String::new();    
-    io::stdin().read_line(&mut buffer).unwrap();
-    buffer.into_bytes()[0]
-}
-
-fn print_error(error_message : &str) {
-    println!("");
-    println!("Error, {}", error_message);
-    println!("");
 }
